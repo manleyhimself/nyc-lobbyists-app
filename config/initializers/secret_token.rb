a246c143c915a76372726deac12f935838327f76
manleyhimself <bcmanley1@gmail.com>
2013-11-27 18:20:54 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NycLobbyistsApp::Application.config.secret_key_base = '82111d7ccf886136a53af64410f9a205608c54c7a1ed5d8476e73805a05e1efa8e33b6db0ff56e30c72879f2a30e8588497b92b12678f73888a3189b491160ff'
